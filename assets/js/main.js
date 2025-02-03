0d114eef94938917186f0fcacd0485d642db1c11
unknown <hellomrbharath@gmail.com>
2025-02-03 12:19:52 +0400

    page for business card

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,69 @@
 */
 (function() {
   "use strict";
+  const canvas = document.getElementById("particleCanvas");
+  if(canvas){
+  const ctx = canvas.getContext("2d");
+  
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
+  let particles = [];
+  
+  class Particle {
+      constructor() {
+          this.x = Math.random() * canvas.width;
+          this.y = Math.random() * canvas.height;
+          this.size = Math.random() * 3 + 1;
+          this.speedX = Math.random() * 1 - 1;
+          this.speedY = Math.random() * 1 - 1;
+      }
+  
+      update() {
+          this.x += this.speedX;
+          this.y += this.speedY;
+  
+          if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+          if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+      }
+  
+      draw() {
+          ctx.fillStyle = "rgba(50,185,232,255)";
+          ctx.beginPath();
+          ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+          ctx.closePath();
+          ctx.fill();
+      }
+  }
+  
+  function init() {
+      for (let i = 0; i < 100; i++) {
+          particles.push(new Particle());
+      }
+  }
+  
+  function animate() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+      particles.forEach((particle) => {
+          particle.update();
+          particle.draw();
+      });
+  
+      requestAnimationFrame(animate);
+  }
+  
+  window.addEventListener("resize", () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      particles = [];
+      init();
+  });
+  
+  init();
+  animate();
+  
+}
 
   /**
    * Easy selector helper function
@@ -269,3 +332,4 @@
   new PureCounter();
 
 })()
+
